6e21e0306e4f6089a85b54b6a45a6e06a535ebc7
-T.K.- <t_k_233@outlook.com>
2023-08-20 20:40:55 -0700

    FIX: fix function arguments

--- examples/example_keysight.py
+++ examples/example_keysight.py
@@ -1,8 +1,12 @@
 import time
 
-from cc.scpi import Keysight33600A
+from cc.scpi import Keysight81134A
 
-device = Keysight33600A("128.32.62.101")
+device = Keysight81134A("128.32.62.102")
 device.connect()
 
 print(device.getInstrumentIdentification())
+
+
+device.enableOutput(Keysight81134A.Channel.CH1)
+device.disableOutput(Keysight81134A.Channel.CH1)
