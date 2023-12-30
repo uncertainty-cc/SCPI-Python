cf27828e21edfc2e6022638dc94fe1cbe0c5ae58
-T.K.- <t_k_233@outlook.com>
2023-12-30 00:09:12 -0800

    FIX: add missing requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[],
+    install_requires=[
+        "cc.serializer",
+    ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
