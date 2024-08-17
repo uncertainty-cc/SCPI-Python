0c3d7554fea093065daeefbd6141a0f66d0ba45b
-T.K.- <t_k_233@outlook.com>
2024-08-16 22:27:18 -0700

    ADD: version bump

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="cc-scpi",
-    version="2023.12.30",
+    name="cc.scpi",
+    version="2024.8.16",
     author="Uncertainty.",
     author_email="t_k_233@outlook.email",
     description="Wrapper of SCPI language for controlling various lab instruments.",
